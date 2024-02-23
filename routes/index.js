e11d9be48364648839f8cba0de4fd3591fb0b4e6
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-02-23 09:23:00 -0400

    add

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,40 @@
 var express = require('express');
 var router = express.Router();
-const range = require('range-parser');
+const rangeParser = require('range-parser');
 const fs = require('fs');
 const path = require('path');
 
-/* GET home page. */
+
 router.get('/', function(req, res, next) {
-  // Rota principal, você pode renderizar sua página HTML aqui se necessário
-  res.render('index', { title: 'Express', src:'UFDP.S09E14.mp4' });
+  res.render('index');
 });
 
+
+router.get('/video', (req, res) => {
+  const videoPath = 'videos/UFDP.S09E14.mp4';
+  const stat = fs.statSync(videoPath); //obter informações estatísticas do arquivo, retorna um objeto fs.Stats, o qual contém informações sobre o arquivo como tamanho, timestamps de modificação, criação, etc.
+  const fileSize = stat.size; //está atribuindo o tamanho do arquivo (em bytes) à variável
+
+  const range = req.headers.range || 'bytes=0-';
+
+  const positions = rangeParser(fileSize, range, { combine: true });
+
+  const start = positions[0].start;
+  const end = positions[0].end;
+
+  const chunkSize = (end - start) + 1;
+
+  const stream = fs.createReadStream(videoPath, { start, end });
+
+  res.writeHead(206, {
+    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': chunkSize,
+    'Content-Type': 'video/mp4',
+  });
+
+  stream.pipe(res);
+});
 
 
 module.exports = router;
