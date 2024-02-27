d6517912657633dcbdb5af1f82011df6eb02bcc0
Seu Nome <MendoncaGabriel1997@gmail.com>
2024-02-27 16:55:53 -0400

    add

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const router = express.Router()
 const indexController = require('../controller/indexController')
@@ -5,5 +6,18 @@ const indexController = require('../controller/indexController')
 router.get('/',  indexController.indexHome)
 router.get('/serie/:id',  indexController.seriePage)
 router.get('/serie/player/:id',  indexController.player)
+
+
+
+router.get('/teste', (req, res) => {
+    const filePath = path.join(__dirname, '../public/images/1709044321344.png');
+    
+    res.sendFile(filePath, (err) => {
+      if (err) {
+        console.error(`Erro ao enviar o arquivo: ${err.message}`);
+        res.status(404).send('Arquivo não encontrado');
+      }
+    });
+  });
 
 module.exports = router
